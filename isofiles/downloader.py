af99253c438d50f6e0fd4c1aa98691e73f5c3c45
Somebody <33503692+human-from-mars@users.noreply.github.com>
2018-01-17 13:02:49 -0800

    Update downloader.py

--- isofiles/downloader.py
+++ isofiles/downloader.py
@@ -2,18 +2,61 @@ import urllib
 import urllib2
 import requests
 
-url = 'insert url here'
+a = 1
+b = 2
+c = 3
 
-print "downloading with urllib"
-urllib.urlretrieve(url, "insert package name here")
+strPath =
+f = open(strPath)
+strText = f.read()
 
-print "downloading with urllib2"
-f = urllib2.urlopen(url)
-data = f.read()
-with open("code2.zip", "wb") as code:
-    code.write(data)
+if strText = a
+    url = 'insert url here'
 
-print "downloading with requests"
-r = requests.get(url)
-with open("code3.zip", "wb") as code:
-    code.write(r.content)
+    print "downloading with urllib"
+    urllib.urlretrieve(url, "insert package name here")
+
+    print "downloading with urllib2"
+    f = urllib2.urlopen(url)
+    data = f.read()
+    with open("code2.zip", "wb") as code:
+        code.write(data)
+
+    print "downloading with requests"
+    r = requests.get(url)
+    with open("code3.zip", "wb") as code:
+        code.write(r.content)
+        
+if strText = b
+    url = 'insert url here'
+
+    print "downloading with urllib"
+    urllib.urlretrieve(url, "insert package name here")
+
+    print "downloading with urllib2"
+    f = urllib2.urlopen(url)
+    data = f.read()
+    with open("code2.zip", "wb") as code:
+        code.write(data)
+
+    print "downloading with requests"
+    r = requests.get(url)
+    with open("code3.zip", "wb") as code:
+        code.write(r.content) 
+        
+if strText = c
+    url = 'insert url here'
+
+    print "downloading with urllib"
+    urllib.urlretrieve(url, "insert package name here")
+
+    print "downloading with urllib2"
+    f = urllib2.urlopen(url)
+    data = f.read()
+    with open("code2.zip", "wb") as code:
+        code.write(data)
+
+    print "downloading with requests"
+    r = requests.get(url)
+    with open("code3.zip", "wb") as code:
+        code.write(r.content)
